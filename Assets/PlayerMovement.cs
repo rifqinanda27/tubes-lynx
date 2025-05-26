c4f770fc8c979ceea92c1d9611af34e8c06f4042
rifqinanda27 <rifqinanda60@gmail.com>
2025-05-26 20:56:20 +0700

    update character

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,59 +2,87 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body;
-    [SerializeField] private float speed;
-    private Animator anim;
-    private bool grounded;
+    public float runSpeed = 10f;
+    public float jumpForce = 15f;
 
-    private void Awake()
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private bool isGrounded = false;
+
+    private int comboStep = 0;
+    private bool canAcceptInput = true; // untuk mencegah input ganda
+
+    void Start()
     {
-
-        body = GetComponent<Rigidbody2D>();
-
-        //Get reference for animator from object
-        anim = GetComponent<Animator>();
-
-        body.freezeRotation = true;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
-
-        //Flip player when moving left-right
-        if (horizontalInput > 0.01f)
+        // Movement hanya jalan kalau tidak attack
+        if (comboStep == 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            float moveInput = Input.GetAxisRaw("Horizontal");
+            rb.linearVelocity = new Vector2(moveInput * runSpeed, rb.linearVelocity.y);
+
+            if (moveInput > 0) spriteRenderer.flipX = false;
+            else if (moveInput < 0) spriteRenderer.flipX = true;
+
+            animator.SetFloat("Speed", Mathf.Abs(moveInput));
         }
-        else if (horizontalInput < -0.01f)
+        else
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            // Saat attack, stop gerak horizontal
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+            animator.SetFloat("Speed", 0);
         }
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        // Lompat
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && comboStep == 0)
         {
-            Jump();
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            animator.SetBool("IsJumping", true);
+            isGrounded = false;
         }
 
-        //Set animator parameters
-        anim.SetBool("HeroKnight_Run", horizontalInput != 0);
-        anim.SetBool("grounded", grounded);
+        /// Input attack
+        if (Input.GetKeyDown(KeyCode.J) && canAcceptInput && isGrounded && comboStep == 0)
+        {
+            comboStep++;
+            if (comboStep > 4) comboStep = 1;
+
+            Debug.Log("ComboStep: " + comboStep); 
+
+            animator.SetInteger("ComboStep", comboStep);
+            canAcceptInput = false; // tolak input selagi animasi belum selesai
+        }
     }
 
-    private void Jump()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-        anim.SetTrigger("Jump");
-        grounded = false;
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+            animator.SetBool("IsJumping", false);
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    // Fungsi ini harus dipanggil oleh Animation Event di akhir setiap animasi attack
+    public void ResetAttack()
     {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
+        comboStep = 0;
+        animator.SetInteger("ComboStep", 0);
+        canAcceptInput = true; // boleh terima input attack baru lagi
+    }
+
+    // Fungsi ini harus dipanggil oleh Animation Event di frame "attack siap terima input berikutnya"
+    // Misal di tengah animasi attack, supaya combo bisa diteruskan
+    public void EnableNextInput()
+    {
+        if (isGrounded)
+            canAcceptInput = true;
     }
 }
