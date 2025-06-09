72f014910f28bef6e932bb621f57788350fbfd42
SadewaNickz <Bagussadewa444@gmail.com>
2025-06-09 15:26:29 +0700

    Nambah Assets Map 1

---
+++ Assets/Script/Bat/Bat_Move.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Bat_Move : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 2f;
+
+    private Vector3 target;
+
+    private void Start()
+    {
+        target = pointB.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.1f)
+        {
+            target = target == pointA.position ? pointB.position : pointA.position;
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
