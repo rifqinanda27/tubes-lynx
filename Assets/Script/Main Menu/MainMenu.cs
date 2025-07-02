5ee0be5db72421703be1816c50fbc424e9909fbf
SadewaNickz <Bagussadewa444@gmail.com>
2025-07-02 23:53:47 +0700

    Kurang Credit, inventory, bug musik map 2 gk jalan

--- Assets/Script/Main Menu/MainMenu.cs
+++ Assets/Script/Main Menu/MainMenu.cs
@@ -26,7 +26,7 @@ public class MainMenuManager : MonoBehaviour
 
     public void StartGame()
     {
-        StartCoroutine(DelayedSceneLoad("Start"));
+        StartCoroutine(DelayedSceneLoad("Cutscene 1"));
     }
 
     public void OpenEnding()
